1c019c8b3d0f9f4fa7a484899fc39ecd9507da68
vterehov207 <v.terehov207@gmail.com>
2019-10-05 17:30:05 -0400

    project with mapping, logi, register on sql-light DB

---
+++ e_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/e570/Desktop/Udemy_Django_course/Udemy_HTML_Dj_execizes/Django_projects/Django_level_5/learning_user_lightDb/e_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
